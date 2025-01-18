88a1b43ab3c6f4bcc7467dd85d8e84aef816d725
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2025-01-18 16:55:57 -0300

    styles updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
-import "./App.module.css";
+import s from "./App.module.css";
 
 const App = () => {
     return (
         <div>
-            <h1>Phonebook</h1>
+            <h1 className={s.title}>Phonebook</h1>
             <ContactForm />
             <SearchBox />
             <ContactList />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./components/App.module.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
